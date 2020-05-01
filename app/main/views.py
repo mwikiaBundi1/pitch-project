bbcec9bf73a924ed898c4a9ed6578e30d052721e
Buttonupd <Dankariuki0101@gmail.com>
2020-05-01 20:33:33 +0300

    Added a function which lets users add a pitch or a quote

--- app/main/views.py
+++ app/main/views.py
@@ -7,4 +7,25 @@ from ..import db, photos
 from .forms import InForm, lamentform, UpdateProfile
 
 
-# @main.route
+@main.route('/')
+def index ():
+
+    message = 'Gone in 60'
+    title = 'Ready? Go!'
+    return render_template('index.html', message=message, title= title)
+
+@main.route('/info', methods = ['GET', 'POST'])
+@login_required
+def new_info():
+    form = InForm
+    if form.validate_on_submit():
+        category = form.category.data
+        info = form.info.data
+        title = form.title.data
+
+        new_info = InForm(title = title. category=category, info=info, user_id = current_user.id)
+         title = 'New Pitch'
+
+         new_pitch.save_pitch()
+         return redirect(url_for(main.index))
+    return render_template('pitch.html', pitch_entry= form)
