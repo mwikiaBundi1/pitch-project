7a01e10bf73747b894c7d36acd9fe60f9c8f9fbc
Buttonupd <Dankariuki0101@gmail.com>
2020-05-01 17:19:38 +0300

    adjusting main view file to accept different core modules inside the app

--- app/main/views.py
+++ app/main/views.py
@@ -1,3 +1,10 @@
 from flask import render_template, redirect, url_for, redirect
 from flask_login import login_required, current_user
+from flask_login import login_required, current_user
+from ..models import User, Comments, Mininfo
+from ..main import main
+from ..import db, photos
+from .forms import InForm, lamentform, UpdateProfile
+
+
 
