8ca65589bc62628e8a360742c57a5d59a6075b49
Buttonupd <Dankariuki0101@gmail.com>
2020-05-01 12:40:39 +0300

    Added a configuration class file which resolves database url and holds the database driver psycopg2

--- app/models.py
+++ app/models.py
@@ -3,3 +3,5 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from . import login_manager
 from datetime import datetime
+
+@login_manager.
--- config.py
+++ config.py
@@ -3,4 +3,27 @@ import os
 class Config:
     SECRET_KEY =  os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://root:buttonupd@localhost/watchlist'
- 
+    UPLOADED_PHOTOS_DEST = 'app/static/photo'
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME =  os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    SUBJECT_PREFIX = 'Gone in 60 seconds'
+    SENDER_EMAIL = 'Dankariuki0101@gmail.com'
+class prodConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABAASE_URL')
+
+
+class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI =  'postgresql+psycopg2://root:buttonupd@localhost/watchlist'
+    DEBUG = True
+
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI= 'postgresql+psycopg2://root:buttonupd@localhost/watchlist'
+
+config_options = {
+    'development': DevConfig,
+    'production': prodConfig,
+    'test': TestConfig
+}
