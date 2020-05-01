a62123ee5e1dceaaad19dced0ef1c3bda787d94f
Buttonupd <Dankariuki0101@gmail.com>
2020-05-01 12:15:38 +0300

    Installed key extensions add initialized them in relevant py files

---
+++ app/__init__.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+
+bootstrap = Bootstrap()
+db = SQLAlchemy()
+
+
+
+
+def create_app(config_name):
+    app = Flask(__name_)
+    bootstrap.init_app(app)
+    db.init_app(app)
+
+
