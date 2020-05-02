e306267c2942f57d7892357d02979ff7cc2d7905
Buttonupd <Dankariuki0101@gmail.com>
2020-05-02 11:10:41 +0300

    something is not right, not sure what but i will

--- app/email.py
+++ app/email.py
@@ -1,12 +1,12 @@
 from flask_mail import Message
-
 from flask import render_template
-
 from . import mail
 
-def mail_message(subject, template, to, **kwargs):
-    subject = 'IN60SECONDS'
-    sender_email = Message(subject, sender= sender_email, recipients=[to])
-    email.body = render_template(template + ".txt,"**kwargs)
-    email.html = render_template(template + ".html", **kwargs)
+def mail_message(subject,template,to,**kwargs):
+    subject_pref = 'If You Had 1 Chance'
+    sender_email = 'Dankariuki0101@gmail.com'
+
+    email = Message(subject, sender=sender_email, recipients=[to])
+    email.body= render_template(template + ".txt",**kwargs)
+    email.html = render_template(template + ".html",**kwargs)
     mail.send(email)
