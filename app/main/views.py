be39933fc87a2ad13f2ba9154d00444775b18215
Buttonupd <Dankariuki0101@gmail.com>
2020-05-02 11:50:10 +0300

    I cant place the submit pitch bug

--- app/main/views.py
+++ app/main/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, redirect, url_for, redirect
+from flask import render_template, redirect, url_for, request
 from flask_login import login_required, current_user
 from flask_login import login_required, current_user
 from ..models import User, Comments, Mininfo
